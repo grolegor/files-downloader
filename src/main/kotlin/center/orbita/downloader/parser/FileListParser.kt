f80141f580dbe8f384d376f2158d1f3dba081262
grol <grolegor@gmail.com>
2019-01-23 03:17:59 +0300

    add validate for FileListParser

--- src/main/kotlin/center/orbita/downloader/parser/FileListParser.kt
+++ src/main/kotlin/center/orbita/downloader/parser/FileListParser.kt
@@ -1,12 +1,31 @@
 package center.orbita.downloader.parser
 
+import center.orbita.downloader.utils.isValidMD5Hex
+import mu.KotlinLogging
+import java.net.MalformedURLException
 import java.net.URL
+
+private val logger = KotlinLogging.logger { }
 
 class FileListParser(private val content: String) {
 
     fun parse(): List<Row> {
-        //todo validate rows
-        return content.lines().map { line -> line.split(",") }.filter { it.size == 2 }.map { Row(URL(it[0].trim()), it[1].trim()) }
+        val rows: MutableList<Row> = mutableListOf()
+        content.lines().map { line -> line.split(",") }.filter { it.size == 2 }.map { Pair(it[0].trim(), it[1].trim()) }.forEach { (urlString, md5) ->
+            if (!md5.isValidMD5Hex()) {
+                logger.warn { "$md5 is not valid md5 for $urlString" }
+            }
+            val url: URL
+            try {
+                url = URL(urlString)
+            } catch (e: MalformedURLException) {
+                logger.error { "$urlString is not valid URL" }
+                return@forEach
+            }
+            rows += Row(url, md5)
+        }
+
+        return rows
     }
 
     class Row(val url: URL, val checkSum: String)
