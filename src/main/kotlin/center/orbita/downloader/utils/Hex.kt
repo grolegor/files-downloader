a60afb5806eb204554d3d62579c141d53f70ff3a
grol <grolegor@gmail.com>
2019-01-23 04:33:14 +0300

    create hexToString extension

--- src/main/kotlin/center/orbita/downloader/utils/Hex.kt
+++ src/main/kotlin/center/orbita/downloader/utils/Hex.kt
@@ -1,3 +1,5 @@
 package center.orbita.downloader.utils
 
+fun ByteArray.hexString() = joinToString("") { String.format("%02x", it) }
+
 fun String.isValidMD5Hex() = matches(Regex("^[a-fA-F0-9]{32}$"))
