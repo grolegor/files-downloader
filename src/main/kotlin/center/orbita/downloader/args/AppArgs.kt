976770a25fe9e715e0e31ab1addcbf7b14c843f0
grol <grolegor@gmail.com>
2019-01-23 03:22:41 +0300

    add description for all params

--- src/main/kotlin/center/orbita/downloader/args/AppArgs.kt
+++ src/main/kotlin/center/orbita/downloader/args/AppArgs.kt
@@ -4,15 +4,18 @@ import com.beust.jcommander.Parameter
 
 class AppArgs {
 
-    @Parameter(names = ["--url"], required = true)
+    @Parameter(names = ["--url"], required = true,
+            description = "URL of the file that contains comma-separated list of links to download and its md5 checksum")
     lateinit var listFilesURL: String
         private set
 
-    @Parameter(names = ["-d", "--outputDirectory"])
+    @Parameter(names = ["-d", "--outputDirectory"],
+            description = "Absolute or relative path to the directory where all files will be downloaded to")
     var outputDirectory: String = ""
         private set
 
-    @Parameter(names = ["--help"], help = true)
+    @Parameter(names = ["--help"], help = true,
+            description = "Print the help")
     var help = false
         private set
 }
